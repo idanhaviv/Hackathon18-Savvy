b3694b7f08953717f697c59e7b04649b4aff37a5
idan <idan@soluto.com>
2018-04-25 11:00:03 +0300

    initial dummy content item getter

---
+++ getContentItem.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const parseString = require('xml2js').parseString;
+
+
+if (process.argv.length <= 2) {
+    console.log("Usage: " + __filename + " keyword");
+    process.exit(-1);
+}
+ 
+var keyword = process.argv[2]
+
+axios.get('https://thewirecutter.com/search/galaxy-s9/feed/rss2/')
+  .then(response => {
+    console.log("Receive response");
+    var xml = response.data;
+    parseString(xml, function (err, result) {
+        console.dir(result.rss.channel[0].item[0]);
+    });
+
+    
+  })
+  .catch(error => {
+    console.log(error);
+  });
